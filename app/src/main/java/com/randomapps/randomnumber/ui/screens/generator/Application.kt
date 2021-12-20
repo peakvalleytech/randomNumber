c9c6cf2e7fe8f0919f99c81a035465d43e1f3b40
peakvalleytech <peakvalleytech@gmail.com>
2021-12-19 19:48:13 -0800

    Add hilt dependencies

---
+++ app/src/main/java/com/randomapps/randomnumber/ui/screens/generator/Application.kt
@@ -0,0 +1,8 @@
+package com.randomapps.randomnumber.ui.screens.generator
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RandomNumberApplication : Application() {
+}
