4630292ff168e04d3f3fb3235506353916f05250
peakvalleytech <peakvalleytech@gmail.com>
2022-06-05 02:30:21 -0700

    Update RandomNumberTopBar

--- app/src/main/java/com/randomapps/randomnumber/ui/common/component/RandomNumberTopBar.kt
+++ app/src/main/java/com/randomapps/randomnumber/ui/common/component/RandomNumberTopBar.kt
@@ -1,14 +1,19 @@
 package com.randomapps.randomnumber.ui.common.component
 
+import androidx.compose.foundation.background
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.randomapps.randomnumber.ui.theme.blue400
 
 @Composable
 fun RandomNumberTopBar() {
     TopAppBar(
-        title = {Text("Random Number")}
+        title = {Text("Random Number")},
+       backgroundColor = blue400
     )
 
 }
