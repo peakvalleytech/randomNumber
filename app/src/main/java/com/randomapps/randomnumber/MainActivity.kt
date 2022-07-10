51019baa3783e164e69aaf87f5deb9ca58ceafc2
peakvalleytech <peakvalleytech@gmail.com>
2022-07-09 17:44:09 -0700

    Implement admob banner

--- app/src/main/java/com/randomapps/randomnumber/MainActivity.kt
+++ app/src/main/java/com/randomapps/randomnumber/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.randomapps.randomnumber
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Surface
+import android.view.View
 import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.randomapps.randomnumber.ui.common.component.RandomNumberTopBar
 import com.randomapps.randomnumber.ui.nav.NavComponent
@@ -28,31 +40,42 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
         MobileAds.initialize(this) {}
+//        val adView = AdView(this)
+//        adView.setAdSize(AdSize.BANNER)
+//        adView.setAdUnitId("ca-app-pub-3142101706002267/5276646677")
+//        addContentView(adView, ViewGroup.LayoutParams(
+//            ViewGroup.LayoutParams.MATCH_PARENT,
+//            ViewGroup.LayoutParams.WRAP_CONTENT,
+//        ))
+//        adView.loadAd(adRequest)
+        val adRequest = AdRequest.Builder().build()
+
         setContent {
             RandomNumberTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = Color.Yellow) {
-                    Scaffold(
-                        topBar = {
-                            RandomNumberTopBar()
+                Column(verticalArrangement = Arrangement.SpaceBetween) {
+                    Surface(border = BorderStroke(3.dp, Color.Cyan), color = Color.Yellow, modifier = Modifier.fillMaxHeight(0.95f)) {
+                        Scaffold(
+                            topBar = {
+                                RandomNumberTopBar()
+                            }
+                        ) {
+                            NavComponent()
                         }
-                    ) {
-                        NavComponent()
                     }
+                    AndroidView(factory = {
+                        AdView(it).apply {
+                            setAdSize(AdSize.BANNER)
+                            setAdUnitId("ca-app-pub-3142101706002267/5276646677")
+                            loadAd(adRequest)
+                        }
+
+                    })
                 }
             }
         }
 
-        val crashButton = android.widget.Button(this)
-        crashButton.text = "Test Crash"
-        crashButton.setOnClickListener {
-            throw RuntimeException("Test Crash") // Force a crash
-        }
 
-        addContentView(crashButton, ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        ))
     }
 }
 
