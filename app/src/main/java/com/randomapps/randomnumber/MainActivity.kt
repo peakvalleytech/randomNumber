3445ee9ed0c26bf34694ebe599f086d062634698
peakvalleytech <peakvalleytech@gmail.com>
2022-07-20 21:28:00 -0700

    Remove imports from MainActivity

--- app/src/main/java/com/randomapps/randomnumber/MainActivity.kt
+++ app/src/main/java/com/randomapps/randomnumber/MainActivity.kt
@@ -1,26 +1,20 @@
 package com.randomapps.randomnumber
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Surface
-import android.view.View
-import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
@@ -28,11 +22,8 @@ import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.randomapps.randomnumber.ui.common.component.RandomNumberTopBar
 import com.randomapps.randomnumber.ui.nav.NavComponent
-import com.randomapps.randomnumber.ui.screens.generator.GeneratorViewModel
 import com.randomapps.randomnumber.ui.theme.RandomNumberTheme
 import dagger.hilt.android.AndroidEntryPoint
-import org.w3c.dom.Text
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
