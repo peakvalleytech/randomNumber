a9ebefd6c5772d8a539de7d9f04de931156b74b4
peakvalleytech <peakvalleytech@gmail.com>
2022-07-21 23:14:44 -0700

    Update NumberGenerator model

--- app/domain/src/main/java/com/randomapps/randomgenerator/domain/models/NumberGenerator.kt
+++ app/domain/src/main/java/com/randomapps/randomgenerator/domain/models/NumberGenerator.kt
@@ -1,7 +1,7 @@
 package com.randomapps.randomgenerator.domain.models
 
 data class NumberGenerator(
-    var id : Long,
+    var id : Long = 0,
     var name : String,
     var from : Int,
     var to : Int
